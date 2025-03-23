5d5de5d9f6a060856ef269e1f2fe5db0ec2cfc53
plodah <mleiper89@gmail.com>
2025-03-23 22:45:40 +0000

    create turbo fire module

---
+++ turbo_fire/turbo_fire.c
@@ -0,0 +1,89 @@
+#include "turbo_fire.h"
+#include "quantum.h"
+
+uint8_t turbo_fire_key_a = TURBO_FIRE_KEY_A;
+uint8_t turbo_fire_key_b = TURBO_FIRE_KEY_B;
+uint8_t turbo_fire_key_c = TURBO_FIRE_KEY_C;
+uint8_t turbo_fire_rate = TURBO_FIRE_RATE;
+
+uint32_t turbo_fire_a(uint32_t trigger_time, void *cb_arg) {
+    tap_code16( turbo_fire_key_a );
+    return turbo_fire_rate;
+}
+uint32_t turbo_fire_b(uint32_t trigger_time, void *cb_arg) {
+    tap_code16( turbo_fire_key_b );
+    return turbo_fire_rate;
+}
+uint32_t turbo_fire_c(uint32_t trigger_time, void *cb_arg) {
+    tap_code16( turbo_fire_key_c );
+    return turbo_fire_rate;
+}
+
+bool process_record_turbo_fire(uint16_t keycode, keyrecord_t *record){
+    switch(keycode) {
+
+        case COMMUNITY_MODULE_TURBO_A_TOGGLE:
+            if(record->event.pressed){
+                if(turbo_fire_a_token == INVALID_DEFERRED_TOKEN ){
+                    turbo_fire_a_token = defer_exec(turbo_fire_rate, turbo_fire_a, NULL);
+                }
+                else{
+                    cancel_deferred_exec(turbo_fire_a_token);
+                    turbo_fire_a_token = INVALID_DEFERRED_TOKEN;
+                }
+            }
+            return false;
+        case COMMUNITY_MODULE_TURBO_A_MOMENTARY:
+            if(record->event.pressed){
+                turbo_fire_a_token = defer_exec(turbo_fire_rate, turbo_fire_a, NULL);
+            }
+            else{
+                cancel_deferred_exec(turbo_fire_a_token);
+                turbo_fire_a_token = INVALID_DEFERRED_TOKEN;
+            }
+            return false;
+
+        case COMMUNITY_MODULE_TURBO_B_TOGGLE:
+            if(record->event.pressed){
+                if(turbo_fire_b_token == INVALID_DEFERRED_TOKEN ){
+                    turbo_fire_b_token = defer_exec(turbo_fire_rate, turbo_fire_b, NULL);
+                }
+                else{
+                    cancel_deferred_exec(turbo_fire_b_token);
+                    turbo_fire_b_token = INVALID_DEFERRED_TOKEN;
+                }
+            }
+            return false;
+        case COMMUNITY_MODULE_TURBO_B_MOMENTARY:
+            if(record->event.pressed){
+                turbo_fire_b_token = defer_exec(turbo_fire_rate, turbo_fire_b, NULL);
+            }
+            else{
+                cancel_deferred_exec(turbo_fire_b_token);
+                turbo_fire_b_token = INVALID_DEFERRED_TOKEN;
+            }
+            return false;
+
+        case COMMUNITY_MODULE_TURBO_C_TOGGLE:
+            if(record->event.pressed){
+                if(turbo_fire_c_token == INVALID_DEFERRED_TOKEN ){
+                    turbo_fire_c_token = defer_exec(turbo_fire_rate, turbo_fire_c, NULL);
+                }
+                else{
+                    cancel_deferred_exec(turbo_fire_c_token);
+                    turbo_fire_c_token = INVALID_DEFERRED_TOKEN;
+                }
+            }
+            return false;
+        case COMMUNITY_MODULE_TURBO_C_MOMENTARY:
+            if(record->event.pressed){
+                turbo_fire_c_token = defer_exec(turbo_fire_rate, turbo_fire_c, NULL);
+            }
+            else{
+                cancel_deferred_exec(turbo_fire_c_token);
+                turbo_fire_c_token = INVALID_DEFERRED_TOKEN;
+            }
+            return false;
+    }
+    return true;
+}
---
+++ turbo_fire/turbo_fire.h
@@ -0,0 +1,33 @@
+#pragma once
+#include "deferred_exec.h"
+
+#ifndef TURBO_FIRE_KEY_A
+    #define TURBO_FIRE_KEY_A KC_A
+#endif // TURBO_FIRE_KEY_A
+
+#ifndef TURBO_FIRE_KEY_B
+    #define TURBO_FIRE_KEY_B KC_B
+#endif // TURBO_FIRE_KEY_B
+
+#ifndef TURBO_FIRE_KEY_C
+    #define TURBO_FIRE_KEY_C KC_C
+#endif // TURBO_FIRE_KEY_C
+
+#ifndef TURBO_FIRE_KEY_D
+    #define TURBO_FIRE_KEY_C KC_D
+#endif // TURBO_FIRE_KEY_D
+
+#ifndef TURBO_FIRE_RATE
+    #define TURBO_FIRE_RATE 25
+#endif // TURBO_FIRE_RATE
+
+deferred_token turbo_fire_a_token;
+deferred_token turbo_fire_b_token;
+deferred_token turbo_fire_c_token;
+deferred_token turbo_fire_d_token;
+
+uint8_t turbo_fire_key_a;
+uint8_t turbo_fire_key_b;
+uint8_t turbo_fire_key_c;
+uint8_t turbo_fire_key_d;
+uint8_t turbo_fire_rate;
