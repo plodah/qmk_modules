42b59c343a2c77f25bb39e0bb9dc288157463cf1
plodah <mleiper89@gmail.com>
2025-11-05 01:18:11 +0000

    wip jiggler - more patterns

--- mouse_jiggler/mouse_jiggler.h
+++ mouse_jiggler/mouse_jiggler.h
@@ -1,11 +1,8 @@
 #pragma once
 
 #include QMK_KEYBOARD_H
+#include <stdint.h>
 #include "deferred_exec.h"
-
-#if (!defined(MSJIGGLER_NOINTRO)) && (!defined(MSJIGGLER_INTRO_TIMEOUT))
-  #define MSJIGGLER_INTRO_TIMEOUT 1400
-#endif // MSJIGGLER_NOINTRO
 
 deferred_token msJigMainToken;
 deferred_token msJigIntroToken;
@@ -15,3 +12,29 @@ void jiggler_end(void);
 void jiggler_start(void);
 void jiggler_toggle(void);
 void jiggle_delay(uint16_t delay_sec);
+
+#if (!defined(MSJIGGLER_NOINTRO)) && (!defined(MSJIGGLER_INTRO_TIMEOUT))
+  #define MSJIGGLER_INTRO_TIMEOUT 1000
+#endif // MSJIGGLER_NOINTRO
+
+#define MSJIGGLER_PATTERN_SUBTLE 1
+#define MSJIGGLER_PATTERN_XLINE 2
+#define MSJIGGLER_PATTERN_YLINE 3
+#define MSJIGGLER_PATTERN_CIRCLE 4
+#define MSJIGGLER_PATTERN_CIRCLESMALL 5
+#define MSJIGGLER_PATTERN_CIRCLECCW 6
+#define MSJIGGLER_PATTERN_CIRCLECCWSMALL 7
+#define MSJIGGLER_PATTERN_FIGURE 8
+#define MSJIGGLER_PATTERN_SQUARE 9
+
+#if ! defined(MSJIGGLER_PATTERN)
+  #define MSJIGGLER_PATTERN MSJIGGLER_PATTERN_SUBTLE
+#endif // defined(MSJIGGLER_PATTERN)
+
+#if ! defined(MSJIGGLER_PATTERN_INTRO)
+  #define MSJIGGLER_PATTERN_INTRO MSJIGGLER_PATTERN_CIRCLESMALL
+#endif // defined(MSJIGGLER_PATTERN)
+
+#if ! defined(MSJIGGLER_PATTERN_ENDING)
+  #define MSJIGGLER_PATTERN_ENDING MSJIGGLER_PATTERN_CIRCLECCWSMALL
+#endif // defined(MSJIGGLER_PATTERN)
