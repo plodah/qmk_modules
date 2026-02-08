031e0d02637a4d78db06715ed74803ddd22b1de5
plodah <plodah@pm.me>
2026-02-08 23:47:03 +0000

    Rework pmw_rotation

--- pmw_rotation/pmw_rotation.c
+++ pmw_rotation/pmw_rotation.c
@@ -1,32 +1,58 @@
 #include QMK_KEYBOARD_H
 #include "pmw_rotation.h"
 
-void pmw_rotation_act (bool absolute, int16_t value) {
-    if (absolute){ pmwrotation = value; }
-    else { pmwrotation += value; }
-    while(! (-128 <= pmwrotation && pmwrotation <= 128)){
-        if (pmwrotation > 128){pmwrotation -= 256 + value;}
-        if (pmwrotation < -128){pmwrotation += 256 - value;}
-    }
-    dprintf("set pmwrotation:%d\n", pmwrotation);
-    pmw33xx_write(0, REG_Angle_Tune, CONSTRAIN(pmwrotation, -127, 127));
+#ifdef ROTATIONAL_TRANSFORM_ANGLE
+    int8_t pmwrotation = ROTATIONAL_TRANSFORM_ANGLE;
+#else
+    int8_t pmwrotation = 0;
+#endif // ROTATIONAL_TRANSFORM_ANGLE
+
+int8_t pmw_rotation_get_sensor (void) {
+    return pmw33xx_read(0, REG_Angle_Tune);
+}
+
+void pmw_rotation_set_sensor (int8_t cpi) {
+    pmw33xx_write(0, REG_Angle_Tune, pmwrotation);
+}
+
+int8_t pmw_rotation_get_config (void) {
+    return pmwrotation;
+}
+
+void pmw_rotation_set_config (bool absolute, int8_t value) {
+    int16_t temp_pmwrotation = pmwrotation;
+    if (absolute){ temp_pmwrotation = value; }
+    else { temp_pmwrotation += value; }
+    pmwrotation = CONSTRAIN(temp_pmwrotation, -PMW_ROTATION_LIMIT, PMW_ROTATION_LIMIT);
+    dprintf("set pmwrotation:%d\n", -pmwrotation);
+}
+
+void pmw_rotation_config_to_sensor (void) {
+    pmw_rotation_set_sensor(pmwrotation);
+}
+
+void pmw_rotation_sensor_to_config (void) {
+    pmwrotation = pmw_rotation_get_sensor();
 }
 
 bool process_record_pmw_rotation (uint16_t keycode, keyrecord_t *record) {
     if(record->event.pressed){
-        dprintf("process_record_pmw_rotation");
+        dprintf("process_record_pmw_rotation\n");
         switch (keycode) {
             case COMMUNITY_MODULE_PMW_ROTATE_CCW:
+                pmw_rotation_set_config(false, PMW_ROTATION_STEP_SIZE);
+                pmw_rotation_config_to_sensor();
                 dprintf(" PMW_ROTATE_CCW\n");
-                pmw_rotation_act(false, -PMW_ROTATION_STEP_SIZE);
                 return false;
             case COMMUNITY_MODULE_PMW_ROTATE_CW:
+                pmw_rotation_set_config(false, -PMW_ROTATION_STEP_SIZE);
+                pmw_rotation_config_to_sensor();
                 dprintf(" PMW_ROTATE_CW\n");
-                pmw_rotation_act(false, PMW_ROTATION_STEP_SIZE);
                 return false;
             case COMMUNITY_MODULE_PMW_ROTATE_RESET:
+                pmw_rotation_set_config(true, ROTATIONAL_TRANSFORM_ANGLE);
+                pmw_rotation_config_to_sensor();
                 dprintf(" PMW_ROTATE_RST\n");
-                pmw_rotation_act(true, ROTATIONAL_TRANSFORM_ANGLE);
                 return false;
             default:
                 return true;
--- pmw_rotation/pmw_rotation.h
+++ pmw_rotation/pmw_rotation.h
@@ -3,15 +3,17 @@
 #include QMK_KEYBOARD_H
 #include "drivers/sensors/pmw33xx_common.h"
 
-#ifdef ROTATIONAL_TRANSFORM_ANGLE
-    int16_t pmwrotation = ROTATIONAL_TRANSFORM_ANGLE;
-#else
-    int16_t pmwrotation = 0;
-#endif // ROTATIONAL_TRANSFORM_ANGLE
-
 #ifndef PMW_ROTATION_STEP_SIZE
-    #define PMW_ROTATION_STEP_SIZE 16
+    #define PMW_ROTATION_STEP_SIZE 15
+#endif
+#ifndef PMW_ROTATION_LIMIT
+    #define PMW_ROTATION_LIMIT 90
 #endif
 
-void pmw_rotation_act (bool absolute, int16_t value);
+int8_t pmw_rotation_get_sensor (void);
+void pmw_rotation_set_sensor(int8_t);
+int8_t pmw_rotation_get_config (void);
+void pmw_rotation_set_config (bool absolute, int8_t value);
+void pmw_rotation_config_to_sensor (void);
+void pmw_rotation_sensor_to_config (void);
 bool process_record_pmw_rotation (uint16_t keycode, keyrecord_t *record);
