a76cc8da25a116a7e0710646faba7faa939df503
plodah <plodah@pm.me>
2026-02-19 03:10:37 +0000

    Rework task_switch with configurable vars

--- task_switch/task_switch.c
+++ task_switch/task_switch.c
@@ -2,19 +2,35 @@
 #include "task_switch.h"
 
 #if defined(TASKSWITCH_MODE_DE)
-    uint32_t taskswitch_reset(uint32_t trigger_time, void *cb_arg) {
-        unregister_code(KC_LALT);
-        is_taskswitch_altregistered = false;
+    deferred_token taskSwitchToken = INVALID_DEFERRED_TOKEN;
+#else // TASKSWITCH_MODE_DE
+    uint16_t taskswitch_timer = 0;
+#endif // defined(TASKSWITCH_MODE_DE)
+
+bool mod_registered;
+uint8_t mod = TASKSWITCH_MOD;
+uint8_t rev_mod = TASKSWITCH_REVERSE_MOD;
+uint16_t tap = (TASKSWITCH_TAP & 0xFF);
+uint16_t delay = TASKSWITCH_DELAY;
+
+void taskswitch_reset(void){
+    mod_registered = false;
+    unregister_mods(mod);
+}
+
+#if defined(TASKSWITCH_MODE_DE)
+    uint32_t taskswitch_deferred_task(uint32_t trigger_time, void *cb_arg) {
+        taskswitch_reset();
         taskSwitchToken = INVALID_DEFERRED_TOKEN;
-        // cancel_deferred_exec(taskSwitchToken);
+        dprintf("taskswitch_deferred_task\n");
         return 0;
     }
 #else // TASKSWITCH_MODE_DE
     void housekeeping_task_task_switch(void) {
-        if (is_taskswitch_altregistered) {
-            if (timer_elapsed(taskswitch_timer) > TASKSWITCH_DELAY) {
-                unregister_code(KC_LALT);
-                is_taskswitch_altregistered = false;
+        if (mod_registered) {
+            if (timer_elapsed(taskswitch_timer) > delay) {
+                taskswitch_reset();
+                dprintf("taskswitch timer elapsed\n");
             }
         }
     }
@@ -26,24 +42,27 @@ void taskswitch_press(bool reverse) {
             cancel_deferred_exec(taskSwitchToken);
         }
     #endif // TASKSWITCH_MODE_DE
-
-    if (!(get_mods() & MOD_MASK_ALT)){
-        is_taskswitch_altregistered = true;
-        register_code(KC_LALT);
+    if (!(get_mods() & mod)){
+        dprintf("taskswitch sends mod: %d\n", mod);
+        mod_registered = true;
+        register_mods(mod);
     }
-    
+    uint16_t sendkc = tap;
     if(reverse){
-        tap_code16(S(KC_TAB));
+        // Remove the standard `mod` from the `rev_mod`
+        // shift calculated mod left 8 bits to create  e.g. `S(KC_NO)`
+        // then add `tap`
+        sendkc = (((rev_mod ^ mod) & rev_mod) << 8) | tap;
     }
-    else{
-        tap_code16(KC_TAB);
-    }
+    dprintf("taskswitch press %d\n", sendkc);
+    tap_code16(sendkc);
 }
 
 void taskswitch_release(void) {
-    if(is_taskswitch_altregistered){
+    dprintf("taskswitch release\n");
+    if(mod_registered){
         #if defined(TASKSWITCH_MODE_DE)
-            taskSwitchToken = defer_exec(TASKSWITCH_DELAY, taskswitch_reset, NULL);
+            taskSwitchToken = defer_exec(delay, taskswitch_deferred_task, NULL);
         #else // TASKSWITCH_MODE_DE
             taskswitch_timer = timer_read();
         #endif // TASKSWITCH_MODE_DE
@@ -71,7 +90,38 @@ bool process_record_task_switch(uint16_t keycode, keyrecord_t *record) {
                 taskswitch_release();
                 return false;
         }
-
     }
     return true;
+}
+
+uint8_t taskswitch_get_mod(void){
+    return mod;
+}
+void taskswitch_set_mod(uint8_t newmod){
+    dprintf("set_mod: %d\n", newmod);
+    mod = newmod;
+}
+
+uint8_t taskswitch_get_rev_mod(void){
+    return rev_mod;
+}
+void taskswitch_set_rev_mod(uint8_t newmod){
+    dprintf("set_rev_mod: %d\n", newmod);
+    rev_mod = newmod;
+}
+
+uint16_t taskswitch_get_tap(void){
+    return tap;
+}
+void taskswitch_set_tap(uint16_t newtap){
+    dprintf("set_tap: %d -> %d\n", newtap, newtap & 0xff);
+    tap = newtap & 0xff;
+}
+
+uint16_t taskswitch_get_delay(void){
+    return delay;
+}
+void taskswitch_set_delay(uint16_t newdelay){
+    dprintf("set_delay: %d\n", newdelay);
+    delay = newdelay;
 }
--- task_switch/task_switch.h
+++ task_switch/task_switch.h
@@ -1,21 +1,37 @@
 #pragma once
 
+#include <sys/types.h>
 #include QMK_KEYBOARD_H
+
 #ifndef TASKSWITCH_DELAY
     #define TASKSWITCH_DELAY 1000
 #endif // TASKSWITCH_DELAY
+
+#ifndef TASKSWITCH_MOD
+    #define TASKSWITCH_MOD MOD_LALT
+#endif // TASKSWITCH_MOD
+
+#ifndef TASKSWITCH_REVERSE_MOD
+    #define TASKSWITCH_REVERSE_MOD MOD_LSFT
+#endif // TASKSWITCH_REVERSE_MOD
+
+#ifndef TASKSWITCH_TAP
+    #define TASKSWITCH_TAP KC_TAB
+#endif // TASKSWITCH_TAP
 
 #if defined(DEFERRED_EXEC_ENABLE) && (!defined(TASKSWITCH_FORCE_NDE))
     #define TASKSWITCH_MODE_DE
     #include "deferred_exec.h"
 #endif // DEFERRED_EXEC_ENABLE
 
-#if defined(TASKSWITCH_MODE_DE)
-    deferred_token taskSwitchToken;
-#else // TASKSWITCH_MODE_DE
-    uint16_t taskswitch_timer;
-#endif // defined(TASKSWITCH_MODE_DE)
-bool is_taskswitch_altregistered;
-
 void taskswitch_press(bool reverse);
 void taskswitch_release(void);
+
+uint8_t taskswitch_get_mod(void);
+void taskswitch_set_mod(uint8_t newmod);
+uint8_t taskswitch_get_rev_mod(void);
+void taskswitch_set_rev_mod(uint8_t newmod);
+uint16_t taskswitch_get_tap(void);
+void taskswitch_set_tap(uint16_t newtap);
+uint16_t taskswitch_get_delay(void);
+void taskswitch_set_delay(uint16_t newdelay);
